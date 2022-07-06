efdd6bd18cabd409e3d1e08d7b46c2fd3e117fc6
tri <tridesignnm@gmail.com>
2022-07-06 23:18:08 +0700

    useEffect ap dung DOM event

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,21 +1,61 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+const tabs = ['posts','comments', 'albums' ]
 
 function Contents() {
     const [title, setTitle] = useState('')
     const [posts, setPost] = useState([])
+    const [type, setType] = useState('posts')
 
+    const [ show, setShow] = useState(false)
+    
+    useEffect( () => {
+        const handleScroll = () => {
+            console.log(window.scrollY)
+        
+            setShow(window.scrollY >= 200)
+            // if(window.scrollY >= 200) {
+            //     console.log(show)
+            //     return setShow(true)
+            // }
+            // else {setShow(false)}
+        
+        }
+
+        window.addEventListener('scroll', handleScroll)
+        // window.addEventListener('resize')
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+            console.log('unmount')
+        }
+    },[])
+    
+   
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get(`https://jsonplaceholder.typicode.com/${type}`)
         .then(res => {
             setPost(res.data);
             // console.log(res.data)
         })
         .catch(error => {console.log(error)} )
-    })
+    },[type])
     return (  
         <div>
+            {tabs.map((tab) =>(
+                <button 
+                    key={tab}
+                    style = {type === tab ? {
+                        color: '#222',
+                        backgroundColor: '#222'
+                    } : {}
+                    }
+                    onClick={ ()=> setType(tab) }
+                >
+                    {tab}
+                </button>
+            ))}
             <h1>hellooo anh em</h1>
             <input 
                 type='text' 
@@ -27,6 +67,17 @@ function Contents() {
                     <li key={post.id}>{post.title}</li>
                 ))}
             </ul>
+            { show && (
+                <button
+                    style={{
+                        position: 'fixed',
+                        right: 20,
+                        bottom: 20
+                    }}  
+                >
+                    gototop
+                </button>
+            ) }
         </div>
 
     );
