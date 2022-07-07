efd0caf2527fd77157eff55f8a99504e0992ebf5
tri <tridesignnm@gmail.com>
2022-07-07 07:36:56 +0700

    user effect timmer

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,83 +1,29 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-const tabs = ['posts','comments', 'albums' ]
 
 function Contents() {
-    const [title, setTitle] = useState('')
-    const [posts, setPost] = useState([])
-    const [type, setType] = useState('posts')
-
-    const [ show, setShow] = useState(false)
+    const [timer, setTimer] = useState(180)
+    
     
     useEffect( () => {
-        const handleScroll = () => {
-            console.log(window.scrollY)
-        
-            setShow(window.scrollY >= 200)
-            // if(window.scrollY >= 200) {
-            //     console.log(show)
-            //     return setShow(true)
-            // }
-            // else {setShow(false)}
-        
-        }
+       const timerId = setInterval( () => {
+        setTimer(timer - 1)
+       },1000)
 
-        window.addEventListener('scroll', handleScroll)
-        // window.addEventListener('resize')
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll)
-            console.log('unmount')
-        }
-    },[])
+       return () => {
+        clearInterval(timerId) 
+    }
+    },[timer]
     
    
-    useEffect(() => {
-        axios.get(`https://jsonplaceholder.typicode.com/${type}`)
-        .then(res => {
-            setPost(res.data);
-            // console.log(res.data)
-        })
-        .catch(error => {console.log(error)} )
-    },[type])
+
+    )
+    
+
     return (  
         <div>
-            {tabs.map((tab) =>(
-                <button 
-                    key={tab}
-                    style = {type === tab ? {
-                        color: '#222',
-                        backgroundColor: '#222'
-                    } : {}
-                    }
-                    onClick={ ()=> setType(tab) }
-                >
-                    {tab}
-                </button>
-            ))}
-            <h1>hellooo anh em</h1>
-            <input 
-                type='text' 
-                value={title}
-                onChange={e => setTitle(e.target.value) }
-            />
-            <ul>
-                {posts.map(post => (
-                    <li key={post.id}>{post.title}</li>
-                ))}
-            </ul>
-            { show && (
-                <button
-                    style={{
-                        position: 'fixed',
-                        right: 20,
-                        bottom: 20
-                    }}  
-                >
-                    gototop
-                </button>
-            ) }
+            {timer}
         </div>
 
     );
