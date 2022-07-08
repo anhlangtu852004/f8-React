5bbc3a4940ac40d3a80b285e71742d3f3196a967
tri <tridesignnm@gmal.com>
2022-07-08 11:48:32 +0700

    router in react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import './App.css';
 // import React from 'react';
-import { useState } from 'react';
+// import { useState } from 'react';
 // import Contents from './components/Contents';
 // import Cart from './components/Cart';
-import PracticeCss from './components/PracticeCss'
+// import PracticeCss from './components/PracticeCss'
+
+import {Routes, Route, Link } from 'react-router-dom'
+
+import HomePage from './components/HomePage';
+import ContactPage from './components/ContactPage';
+import AboutPage from './components/AboutPage';
 
 function App() {
-  const [show, setShow] =useState(false)
+  // const [show, setShow] =useState(false)
 
-  const handleShow = () => show ? setShow(false) : setShow(true)
+  // const handleShow = () => show ? setShow(false) : setShow(true)
   
   return (
     <div>
-      <button onClick={handleShow}>tongle</button>
-      {show && <PracticeCss />}
+      {/* <button onClick={handleShow}>tongle</button> */}
+      {/* {show && <PracticeCss />} */}
+      <ul>
+        <li><Link to='/'>home</Link></li>
+        <li><Link to='/about'>about</Link></li>
+        <li><Link to='/contact'>contact</Link></li>
+      </ul>
 
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/contact' element={<ContactPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/ContactPage.js
@@ -0,0 +1,8 @@
+const ContactPage = () => {
+    return ( 
+        <h1> Contact  page</h1>
+     );
+}
+ 
+export default ContactPage
+;
